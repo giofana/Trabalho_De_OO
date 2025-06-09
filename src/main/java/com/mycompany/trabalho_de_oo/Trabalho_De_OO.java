541124a7d41c93f179c844e7636dac327da80a4e
giovana <giovana.maieli@codejr.com.br>
2025-06-09 16:54:30 -0300

    configurações iniciais

---
+++ src/main/java/com/mycompany/trabalho_de_oo/Trabalho_De_OO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalho_de_oo;
+
+/**
+ *
+ * @author gigio
+ */
+public class Trabalho_De_OO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
